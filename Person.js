633112b81dba7dd5ca4c4ee161c95173ed88aedb
Aphelele2 <aphelelejoyi@gmail.com>
2023-07-31 15:04:49 +0200

    done

---
+++ Person.js
@@ -0,0 +1,9 @@
+// Person constructor function
+function Person(firstName, lastName, emailAddress) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.emailAddress = emailAddress;
+  }
+  
+  module.exports = Person;
+  
---
+++ index.js
@@ -0,0 +1,9 @@
+const Person = require('./Person');
+
+const person1 = new Person('Aphelele', 'Joyi', 'aphelelejoyi@gmail.com');
+const person2 = new Person('Siyamanga', 'Zweni', 'siyamangazweni@gmail.com');
+const person3 = new Person('Kamva', 'Sikalana', 'kamvasikalana@gmail.com');
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
